1379637a2ac7c0ca72c650f36ecc20ecd52ef126
tactics177 <63974953+tactics177@users.noreply.github.com>
2024-06-03 11:58:47 +0200

    first commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { HealthCheckModule } from './health-check/health-check.module';
+import { HealthCheckResolver } from './health-check/health-check.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HealthCheckModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, HealthCheckResolver],
+})
+export class AppModule {}
