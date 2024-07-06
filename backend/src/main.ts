d307f5d9b18e124cab064820621d7fdc3699b425
tactics177 <63974953+tactics177@users.noreply.github.com>
2024-07-06 17:02:06 +0200

    use env variables for urls

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // React app URL
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001', // React app URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
